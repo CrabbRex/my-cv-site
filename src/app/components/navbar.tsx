7bb5f8557ac01c2e5465a5ba42d3318ad57d2ac9
Rex <crabbrex@gmail.com>
2025-07-12 15:00:41 +1000

    Implement Dynamic NavBar.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,8 +3,10 @@
 import Link from 'next/link';
 import ThemeSwitcher from './ThemeSwitcher';
 import { useEffect, useState } from "react";
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+    const pathname = usePathname();
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
@@ -13,16 +15,24 @@ export default function Navbar() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const linkClasses = (href: string) => {
+        const isActive = pathname === href;
+        return `px-3 py-1 rounded-4xl transition border-2 
+            ${isActive 
+                ? 'border-[var(--color-nav-base)] bg-[var(--color-background)]' 
+                : 'border-[var(--color-nav-highlight)] hover:border-transparent'}`;
+    };
+
     return (
         <header className="fixed top-0 left-0 w-full z-50 flex justify-center">
             <div
                 className={`rounded-3xl px-4 py-2 mt-4 transition-all duration-300 backdrop-blur-md`}
             >
                 <nav className="max-w-5xl mx-auto flex gap-4 justify-center">
-                    <Link href="/" className="px-3 py-1 rounded-4xl transition border-2 hover:border-transparent hover:bg-[var(--color-background)]">Home</Link>
-                    <Link href="/projects" className="px-3 py-1 rounded-4xl transition border-2 hover:border-transparent hover:bg-[var(--color-box-hover)">Projects</Link>
-                    <Link href="/cv" className="px-3 py-1 rounded-4xl transition border-2 hover:border-transparent hover:bg-[var(--color-box-hover)">CV</Link>
-                    <Link href="/about" className="px-3 py-1 rounded-4xl transition border-2 hover:border-transparent hover:bg-[var(--color-box-hover)">About</Link>
+                    <Link href="/" className={linkClasses('/')}>Home</Link>
+                    <Link href="/projects" className={linkClasses('/projects')}>Projects</Link>
+                    <Link href="/cv" className={linkClasses('/cv')}>CV</Link>
+                    <Link href="/about" className={linkClasses('/about')}>About</Link>
 
                 </nav>
             </div>
