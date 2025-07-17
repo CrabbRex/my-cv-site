a88e5f62c17c87e1c239a2097fbd1ffda2aa79f9
Rex <crabbrex@gmail.com>
2025-07-17 15:58:10 +1000

    Update About page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ const about = () => {
         {/* Left */}
         <div>
           <Image 
-            src="/Profile.jpg"
+            src="/about.jpg"
             alt="Rex Crabb"
             width={300}
             height={450}
@@ -21,30 +21,18 @@ const about = () => {
         </div>
         {/* Right */}
         <div>
-          <p className="pt-20 max-w-xl">I'm an IT student at Macquarie University who's interested in Web Development and Software Development.</p>
+          <p className="max-w-xl">I'm an IT student at Macquarie University who's interested in Web Development and Software Development.</p>
           <p className="pt-20 max-w-xl">My personal hobbies include reading Science Fiction, Fantasy and Philosophy. I also enjoy exercising, guitar/Rock music and AFL.</p>
         </div>
       </section>
 
       <h1 className="text-8xl pt-30 font-bold mb-4 justify-center text-center">Contact Me:</h1>
       <section className="max-w-2xl mx-auto grid grid-cols-2 md:grid-cols-2 gap-6 items-center text-center pb-20 pt-10">
-        <div>
-          <a
-            href=""
-            target="_blank"
-            className="block p-4 rounded-xl shadow-md hover:shadow-lg transition duration-300"
-            >
-              <h2>Email</h2>
-          </a>
+        <div className="bg-[var(--color-pad)] hover:bg-[var(--color-pad-hover)] rounded-2xl shadow-md hover:shadow-lg transition duration-300 p-6 mt-5 flex flex-col justify-between">
+          <h2><a href="mailto:Crabbrex@gmail.com" className="hover:cursor-pointer">Crabbrex@gmail.com</a></h2>
         </div>
-        <div>
-          <a
-            href=""
-            target="_blank"
-            className="block p-4 rounded-xl shadow-md hover:shadow-lg transition duration-300"
-            >
-              <h2>Phone</h2>
-          </a>
+        <div className="bg-[var(--color-pad)] hover:bg-[var(--color-pad-hover)] rounded-2xl shadow-md hover:shadow-lg transition duration-300 p-6 mt-5 flex flex-col justify-between">
+          <h2>0411 744 688</h2>
         </div>
       </section>
     </main>
