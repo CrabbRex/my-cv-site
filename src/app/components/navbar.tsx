de0848122a5ae87a0a831d45574313b805b7cb71
Rex <crabbrex@gmail.com>
2025-07-19 15:49:49 +1000

    Fix: NavBar now shows active link instead of hooked state.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,6 @@
 // File: src/app/components/navbar.tsx
 "use client"
 import Link from 'next/link';
-import { useEffect, useState } from "react";
 import { usePathname } from 'next/navigation';
 import { motion } from "motion/react";
 
@@ -12,9 +11,8 @@ const navLinks = [
         { href: '/about', label: 'About' }
     ]
 
-
 export default function Navbar() {
-    let [activeTab, setActiveTab] = useState(navLinks[0].href);
+    const pathname = usePathname();
     return (
         <header className="fixed top-0 left-0 w-full z-50 flex justify-center">
             <div className="flex space-x-1 rounded-3xl px-4 py-2 mt-4 backdrop-blur-md">
@@ -22,9 +20,9 @@ export default function Navbar() {
                     <Link
                         key={link.href}
                         href={link.href}
-                        onClick={() => setActiveTab(link.href)}
+                        //onClick={() => setActiveTab(link.href)}
                         className={`relative rounded-full px-3 py-1.5 text-sm font-medium outline-sky-400 transition focus-visible:outline-2
-                            ${activeTab === link.href 
+                            ${pathname === link.href 
                                 ? "text-white" 
                                 : 'border border-transparent hover:border hover:border-[var(--color-foreground)]'
                                 }`}
@@ -32,7 +30,7 @@ export default function Navbar() {
                                     WebkitTapHighlightColor: 'transparent',
                                 }}
                     >
-                        {activeTab === link.href && (
+                        {pathname === link.href && (
                             <motion.span
                                 layoutId="bubble"
                                 className="absolute inset-0 z-10 bg-[var(--color-oppositeBackground)] rounded-full"
@@ -40,12 +38,11 @@ export default function Navbar() {
                                 transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
                             />
                         )}
-                        <span className={`relative z-20 ${activeTab === link.href ? "text-[var(--color-background)]" : ""}`}>
+                        <span className={`relative z-20 ${pathname === link.href ? "text-[var(--color-background)]" : ""}`}>
                             {link.label}
                         </span>
                     </Link>
                 ))}
-                {/* <ThemeSwitcher /> */}
             </div>
         </header>
     )
